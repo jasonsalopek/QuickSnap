4f7816618b7466b539a85274c204a149ef5d72b2
jasonsalopek <2074842@student.swin.edu.au>
2015-08-21 17:07:41 +1000

    add sound effect

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -10,7 +10,10 @@ namespace CardGames
         {
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
+			SwinGame.LoadFontNamed ("GameFont", "Chunckfive.otf", 24);
             SwinGame.BitmapSetCellDetails (cards, 167, 250, 13, 5, 53);      // set the cells in the bitmap to match the cards
+			SwinGame.LoadSoundEffectNamed("Slap", "slap.wav");
+			SwinGame.PlaySoundEffect ("Slap");
         }
 
 		/// <summary>
@@ -51,6 +54,7 @@ namespace CardGames
 		private static void DrawGame(Snap myGame)
 		{
 			SwinGame.DrawBitmap("cardsBoard.png", 0, 0);
+			SwinGame.DrawText ("" + myGame.Score (0), Color.White, "GameFont", 0, 30);
 
 			// Draw the top card
 			Card top = myGame.TopCard;
